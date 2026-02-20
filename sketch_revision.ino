60c653618b957d9adf31937d4c485af04e346d8e
Rhesiana Putri Dewi <167621613+rhebahan@users.noreply.github.com>
2026-02-20 16:59:22 +0700

    Clean up blank lines in sketch_revision.ino
    Removed unnecessary blank lines to improve code readability.

--- sketch_revision.ino
+++ sketch_revision.ino
@@ -14,7 +14,6 @@ void setup() {
   lcd.backlight();                 
   delay(2000);
 
-
   efek("something", 0, 0, 540);
   efek("about", 0, 1, 500);
   efek("you", 6, 1, 675);
@@ -29,67 +28,56 @@ void setup() {
   efek("remember", 0, 1, 1040);
   lcd.clear();
 
-
   efek("it's", 0, 0, 250);
   efek("the", 5, 0, 250);
   efek("same", 0, 1, 550);
   lcd.clear();
 
-  
   efek("damn", 0, 0, 550);
   efek("thing", 0, 1, 670);
   lcd.clear();
 
-  
   efek("that", 0, 0, 250);
   efek("made", 0, 1, 550);
   lcd.clear();
-
   
   efek("my", 0, 0, 300);
   efek("heart", 3, 0, 470);
   efek("surrender", 0, 1, 1110);
   lcd.clear();
 
-  
   efek("and", 0, 0, 250);
   efek("i", 4, 0, 200);
   efek("miss", 0, 1, 240);
   efek("you", 5, 1, 275);
   lcd.clear();
-
   
   efek("on", 0, 0, 250);
   efek("a", 0, 1, 250);
   efek("train", 2, 1, 470);
   lcd.clear();
-
   
   efek("i'll", 0, 0, 430);
   efek("miss", 0, 1, 700);
   efek("you", 5, 1, 350);
   lcd.clear();
 
-  
   efek("in", 0, 0, 350);
   efek("the", 3, 0, 350);
   efek("morning", 0, 1, 350);
   lcd.clear();
-
   
   efek("i", 0, 0, 1100);
   efek("never", 2, 0, 450);
   efek("know", 0, 1, 550);
   efek("what", 5, 1, 250);
   lcd.clear();
-
   
   efek("to", 0, 0, 350);
   efek("think", 3, 0, 450);
   efek("about", 0, 1, 1000);
   lcd.clear();
   delay(250);
-
   
   efek("i", 0, 0, 400);
   efek("think", 2, 0, 500);
@@ -101,7 +89,6 @@ void setup() {
   efek(".", 6, 0, 300);
   efek(".", 7, 0, 300);
   efek(".", 8, 0, 300);
-
 }
 
 void loop() {
